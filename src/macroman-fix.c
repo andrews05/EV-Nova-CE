9041ad2877d0fce5da1e45cba6c03624dc3bc559
andrews05 <andrews05@gmail.com>
2023-10-07 08:18:57 +1300

    Clear call to libiconv_open

--- src/macroman-fix.c
+++ src/macroman-fix.c
@@ -5,6 +5,10 @@
 // Fixes display of MacRoman characters that are not ANSI compatible
 
 
+// Clear CALL to libiconv_open
+// This is not necessary for the patch but the call is no longer required so we may as well get rid of it
+CLEAR(0x004BC43C, 0x90, 0x004BC442);
+
 // Replace CALL to alloc
 CALL(0x004BC783, _alloc_Wide);
 
