40abdf9a9cf4c1b40f64a9990928a6fb385701a9
andrews05 <andrews05@gmail.com>
2023-10-07 13:57:41 +1300

    Minor fixes

--- src/dynamic-resolution.c
+++ src/dynamic-resolution.c
@@ -5,23 +5,25 @@
 // Allows dynamic resolution from cnc ddraw.ini or current display metrics
 
 
-#define FLAGS_FULLSCREEN 0x01 // Nova sets this from prefs
-#define FLAGS_UNKNOWN_1  0x02 // Nova always sets this
-#define FLAGS_BORDERLESS 0x20 // Nova never sets this
-#define FLAGS_ENUMERATE_DD_DEVICES 0x10000 // Exact meaning uncertain; Nova never sets this
+#define NV_SCREEN_FULLSCREEN 0x01 // Nova sets this from prefs
+#define NV_SCREEN_UNKNOWN_1  0x02 // Nova always sets this
+#define NV_SCREEN_BORDERLESS 0x20 // Nova never sets this
+#define NV_SCREEN_ENUMERATE_DD_DEVICES 0x10000 // Exact meaning uncertain; Nova never sets this
 
 
 // Replace CALL to InitializeMonitor
 CALL(0x0041629B, _InitializeMonitor);
 
 void InitializeMonitor() {
-    // Get the full path to ddraw.ini in the current directy
+    // Get the full path to ddraw.ini in the current directory
     LPSTR ddrawIni = GlobalAlloc(0, 4096);
     GetFullPathNameA("ddraw.ini", 4096, ddrawIni, NULL);
 
     // Get width and height from the ini
     int width = GetPrivateProfileIntA("ddraw", "width", 0, ddrawIni);
     int height = GetPrivateProfileIntA("ddraw", "height", 0, ddrawIni);
+
+    GlobalFree(ddrawIni);
 
     // Fallback to current display metrics
     if (width == 0) {
@@ -33,5 +35,5 @@ void InitializeMonitor() {
 
     // Proceed with setup call
     // Fullscreen is forced on as exact behavior will be controlled by cnc ddraw
-    nv_SetupScreen(width, height, 15, FLAGS_FULLSCREEN | FLAGS_UNKNOWN_1);
+    nv_SetupScreen(width, height, 15, NV_SCREEN_FULLSCREEN | NV_SCREEN_UNKNOWN_1);
 }
--- src/font-substitution-fix.asm
+++ src/font-substitution-fix.asm
@@ -1,6 +1,6 @@
 %include "macros/patch.inc"
 %include "macros/datatypes.inc"
 
-; Disables font substitution so that, e.g., Geneva won't be replace with Arial
+; Disables font substitution so that, e.g., Geneva won't be replaced with Arial
 
 @SJMP 0x004BC474, 0x004BC4D0
