41304bd838b601ee7faa4ed558df7ece255d8dc0
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2023-10-09 09:20:32 +0200

    sync with petool repo - remove old license (New one = BSD0)

--- inc/nova.h
+++ inc/nova.h
@@ -1,10 +1,8 @@
 #ifndef NOVA_H 
 #define NOVA_H
 
-// This header is used for patches
+// This header works with sym.asm which defines the Vanilla symbols
 // This header will be split up as it becomes larger
-
-
 
 // ### Variables ###
 
