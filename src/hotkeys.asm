946ffbd574ca48693b751f96bbdbbbe33e224196
andrews05 <andrews05@gmail.com>
2023-10-24 19:37:20 +1300

    More map hotkeys

--- src/hotkeys.asm
+++ src/hotkeys.asm
@@ -23,3 +23,20 @@
     JZ @HOOKEND
     ; Redo the jump we replaced
     JMP 0x004959EF
+
+
+; Map: C = Clear Route (normally centers map, but spacebar does this also)
+;      B = Show/Hide Borders
+@HOOK 0x004A7674, 0x004A767A
+    JNZ .next
+    ; Clear button is item 8
+    MOV WORD[EBX], 8
+    ; Jump to return
+    JMP 0x004A7705
+.next:
+    CMP EAX, 'b'
+    JNZ @HOOKEND
+    ; Borders button is item 9
+    MOV WORD[EBX], 9
+    ; Jump to return
+    JMP 0x004A7705
