83e20fee99926f6b01660ea3221fecbec913004d
FunkyFr3sh <cc.red.alert.1@googlemail.com>
2023-10-27 03:13:01 +0200

    rename WinMain to prepare for c++ support

--- src/winmain.c
+++ src/winmain.c
@@ -4,12 +4,12 @@
 #include "imports.h"
 
 
-CALL(0x00503FE5, _WinMain);
+CALL(0x00503FE5, _fake_WinMain);
 
-int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
+int APIENTRY fake_WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
 {
     if (!imports_init())
         return 0;
 
-    return nv_WinMain(hInst, hInstPrev, cmdline, cmdshow);
+    return WinMain(hInst, hInstPrev, cmdline, cmdshow);
 }
