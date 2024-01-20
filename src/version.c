57b25e997722ef1f9d8d0c4a71a6ace5cad6eca9
andrews05 <andrews05@gmail.com>
2024-01-20 16:47:01 +1300

    Adjust version text color

--- src/version.c
+++ src/version.c
@@ -10,9 +10,9 @@ char version[] = "EV Nova Community Edition r2";
 CALL(0x004884BC, _drawBackground);
 void drawBackground(NVBitmap *bitmap, QDRect *frame) {
     nv_DrawPict(bitmap, frame);
-    g_nv_currentContext->fgColor.red = 0x9999;
-    g_nv_currentContext->fgColor.green = 0x9999;
-    g_nv_currentContext->fgColor.blue = 0x9999;
+    g_nv_currentContext->fgColor.red = 0x8000;
+    g_nv_currentContext->fgColor.green = 0x8000;
+    g_nv_currentContext->fgColor.blue = 0x8000;
     g_nv_currentContext->posX = 5;
     g_nv_currentContext->posY = g_nv_currentContext->bitmap.bounds.bottom - 6;
     g_nv_currentContext->fontFace = g_nv_mainFontFace;
