d61048a0f2d4b51ff5ac49e1c9f8e4cca22056f6
andrews05 <andrews05@gmail.com>
2024-01-21 18:52:43 +1300

    Update patch notes

--- src/debug-mode.c
+++ src/debug-mode.c
@@ -2,45 +2,7 @@
 #include "macros/patch.h"
 #include "nova.h"
 
-// Allows setting debug mode from cnc ddraw.ini
-
-
-/*
-Debug Mode Effects:
-- Show ids/raw values in various dialogs
-- Always have afterburner
-- 50x faster armor regen
-- Ignore capture odds (both player and AI)
-- Skip combat rating requirement for domination
-
-Debug Mode Hotkeys:
-Alt+Home:      Enter debug mode
-Hold Ctrl:     Show target debug info
-Shift+4:       +50k credits
-Alt+Shift+4:   -10k credits
-Ins:           Spawn ship
-Alt+Ins:       Spawn fleet 129 (Federation)
-Alt+Ctrl+Ins:  Spawn fleet 135 (Pirates)
-Alt+Shift+Ins: Spawn fleet 149 (Rebels)
-Ctrl+Ins:      Spawn pers 641 (Hannah Chick)
-Del:           Delete target
-PageUp:        Cycle ship type forwards
-PageDn:        Cycle ship type backwards
-F6:            Restore stats and ammo, activate rank 147 (Hypergate Access)
-Alt+F6:        Same as above, but empty fuel
-F7:            Make target hostile
-Alt+F7:        Make target neutral and leave system
-F8:            Disable target
-Alt+F8:        Assign target as hired escort
-F9:            Destroy target
-Shift+F9:      Destroy target as player (bear the consequences)
-Alt+F9:        Disable player
-F10:           Explore map
-Alt+F10:       Unexplore map
-Backspace:     Complete jump instantly
-Home:          Restart after death (this works even when not in debug mode)
-End:           Exit debug mode
-*/
+// Allows activating debug mode with keyboard shortcut
 
 
 // Replace a CALL to KeyCheck
@@ -70,7 +32,7 @@ int NumberToString(char *buffer, int value, int flags) {
 CALL(0x00452745, 0x004637A0);
 
 
-// Remap keys to be usable on modern keyboards, including laptops
+// Remap keys to be usable on modern keyboards, especially those without a numpad
 
 // Explore map: F10 (was Numpad= on Mac)
 SETBYTE(0x004507B4 + 1, 0x44);
