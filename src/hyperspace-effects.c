015f53c3852523cb24c2101018437b47d36bbd1b
andrews05 <andrews05@gmail.com>
2024-03-11 16:45:58 +1300

    Also change flash for hypergates and wormholes

--- src/hyperspace-effects.c
+++ src/hyperspace-effects.c
@@ -8,8 +8,10 @@
 // Clear call to hide the hyperspace effects checkbox
 CLEAR(0x00488778, 0x90, 0x00488778 + 5);
 
-// Replace call to set the color of the flash
-CALL(0x0044F729, _SetFlashColor);
+// Replace calls to set the color of the flash
+CALL(0x0044F729, _SetFlashColor); // Jump
+CALL(0x00456767, _SetFlashColor); // Hypergate
+CALL(0x00456F27, _SetFlashColor); // Wormhole
 void SetFlashColor(QDColor *color) {
     // If no hyperspace effects, use black instead of the default white
     g_nv_currentContext->fgColor = g_nv_noHyperspaceEffects ? (QDColor){} : *color;
