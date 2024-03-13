9305aadbc3e50111fd4b6fe4c4b26b1b74c37489
andrews05 <andrews05@gmail.com>
2024-03-14 11:27:20 +1300

    Store app path in registry so it can be found by scripts

--- src/winmain.c
+++ src/winmain.c
@@ -13,11 +13,27 @@ int APIENTRY fake_WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, in
     if (!imports_init())
         return 0;
 
+    // Store the app path in the registry
+    HKEY appPath;
+    RegCreateKeyExA(
+        HKEY_CURRENT_USER,
+        "Software\\Microsoft\\Windows\\CurrentVersion\\App Paths\\EV Nova.exe",
+        0,
+        NULL,
+        REG_OPTION_NON_VOLATILE,
+        KEY_WRITE,
+        NULL,
+        &appPath,
+        NULL
+    );
+    char exePath[MAX_PATH];
+    GetModuleFileName(NULL, exePath, sizeof(exePath));
+    RegSetValueExA(appPath, NULL, 0, REG_SZ, (BYTE *)exePath, sizeof(exePath));
+
     // Get full path to ddraw.ini
-    GetModuleFileName(NULL, g_iniPath, MAX_PATH);
     char drivePath[10];
     char dirPath[MAX_PATH];
-    _splitpath(g_iniPath, drivePath, dirPath, NULL, NULL);
+    _splitpath(exePath, drivePath, dirPath, NULL, NULL);
     _makepath(g_iniPath, drivePath, dirPath, "ddraw", "ini");
 
     return WinMain(hInst, hInstPrev, cmdline, cmdshow);
