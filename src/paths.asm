37892075b334724aa0c4234eaf2f2c9d6ea23351
andrews05 <andrews05@gmail.com>
2024-03-14 05:09:07 +1300

    Add one more path fix

--- src/paths.asm
+++ src/paths.asm
@@ -5,6 +5,7 @@
 ; This allows loading data from an alternate directory
 
 ; Add 2 to the existing string offsets to skip the "9:" prefix
+@SET 0x004d2b05, { PUSH 0x0056f2fc + 2} ; Nova Files:Nova Music.mp3, check file exists
 @SET 0x0049d85d, { PUSH 0x0056d1a0 + 2} ; Nova Plug-Ins, shipyard movie
 @SET 0x0049d8da, { PUSH 0x0056d1b4 + 2} ; Nova Files, shipyard movie
 @SET 0x0049db42, { PUSH 0x0056d1a0 + 2} ; Nova Plug-Ins, desc movie
