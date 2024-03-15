b60485373bfc92632a4a3cf7506e160631d3de10
andrews05 <andrews05@gmail.com>
2024-03-15 17:17:57 +1300

    Remove warning about missing music file

--- src/paths.asm
+++ src/paths.asm
@@ -5,7 +5,6 @@
 ; This allows loading data from an alternate directory
 
 ; Add 2 to the existing string offsets to skip the "9:" prefix
-@SET 0x004d2b05, { PUSH 0x0056f2fc + 2} ; Nova Files:Nova Music.mp3, check file exists
 @SET 0x0049d85d, { PUSH 0x0056d1a0 + 2} ; Nova Plug-Ins, shipyard movie
 @SET 0x0049d8da, { PUSH 0x0056d1b4 + 2} ; Nova Files, shipyard movie
 @SET 0x0049db42, { PUSH 0x0056d1a0 + 2} ; Nova Plug-Ins, desc movie
@@ -15,3 +14,6 @@
 
 @SET 0x004bd0f4, { MOV AX, [0x0056ddc9 + 2]} ; // Pilots
 @SET 0x004bd182, { MOV AX, [0x0056ddc9 + 2]} ; // Pilots
+
+; Remove warning about missing music file
+@CLEAR 0x004d2b1e, 0x90, 0x004d2b1e + 5
