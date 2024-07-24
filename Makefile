7e857ed783616181211f9ff5afd4b001022f2ca8
andrews05 <andrew@digerati.co.nz>
2024-07-24 16:22:25 +1200

    Latest petool (#8)
    ---------
    
    Co-authored-by: FunkyFr3sh <cc.red.alert.1@googlemail.com>

--- Makefile
+++ Makefile
@@ -3,18 +3,18 @@
 INPUT       = EV_Nova.dat
 OUTPUT      = EV\ Nova.exe
 LDS         = EV_Nova.lds
-GCCVERSION  = $(shell $(CC) --version | grep ^$(CC) | sed 's/^.* //g')
-IMPORTS     = 0x18F000 7670
-LDFLAGS     = --section-alignment=0x1000 --subsystem=windows --enable-stdcall-fixup --disable-dynamicbase --disable-nxcompat --disable-reloc-section
-NFLAGS      = -f elf -Iinc/
-CFLAGS      = -std=c99 -Iinc/ -O2 -march=i486 -Wall
-CXXFLAGS    = -Iinc/ -O2 -march=i486 -Wall
+
+LDFLAGS     = -Wl,--subsystem=windows -Wl,--disable-nxcompat -Wl,--disable-reloc-section -Wl,--enable-stdcall-fixup -static -nostdlib
+ASFLAGS     = -Iinc
+NFLAGS      = -Iinc -f elf
+CFLAGS      = -Iinc -O2 -march=i486 -Wall
+CXXFLAGS    = -Iinc -O2 -march=i486 -Wall
+
 LIBS        = -luser32 -ladvapi32 -lshell32 -lmsvcrt -lkernel32 -lgdi32 -lcnc_ddraw
-#CXXLIBS     = =./lib/crt2.o -lstdc++ -lgcc -lpthread -lmingw32 -lmoldname -lmingwex -lgcc
 
 OBJS        = rsrc.o \
 			  sym.o \
-			  imports.o \
+			  src/imports.o \
 			  src/winmain.o \
 			  src/debug-mode.o \
 			  src/dynamic-resolution.o \
@@ -40,29 +40,33 @@ OBJS        = rsrc.o \
 
 CC          = i686-w64-mingw32-gcc
 CXX         = i686-w64-mingw32-g++
+AS          = i686-w64-mingw32-as
+STRIP      ?= i686-w64-mingw32-strip
 WINDRES    ?= i686-w64-mingw32-windres
 PETOOL     ?= petool
-STRIP      ?= strip
 NASM       ?= nasm
+
+IMPORTS     = 1  0x18F000 7670
+TLS         = 9  0x0 0
+IAT         = 12 0x0 0
 
 all: $(OUTPUT)
 
 %.o: %.asm
 	$(NASM) $(NFLAGS) -o $@ $<
 
-%.o: %.cpp
-	$(CXX) $(CXXFLAGS) -c -o $@ $<
+%.o: %.rc
+	$(WINDRES) $(WFLAGS) $< $@
 
 rsrc.o: $(INPUT)
 	$(PETOOL) re2obj $(INPUT) $@
 
 $(OUTPUT): $(LDS) $(INPUT) $(OBJS)
-	$(LD) $(LDFLAGS) -T $(LDS) -o "$@" $(OBJS) $(CXXLIBS) $(LIBS) -L=./../lib/gcc/i686-w64-mingw32/$(GCCVERSION) -L=./lib/gcc/i686-w64-mingw32/$(GCCVERSION) -L./lib
-ifneq (,$(IMPORTS))
-	$(PETOOL) setdd "$@" 1 $(IMPORTS) || ($(RM) "$@" && exit 1)
-endif
-	$(PETOOL) setdd "$@" 9 0 0 || ($(RM) "$@" && exit 1)
-	$(PETOOL) setdd "$@" 12 0 0 || ($(RM) "$@" && exit 1)
+	$(CXX) $(LDFLAGS) -T $(LDS) -o "$@" $(OBJS) $(LIBS)
+	$(PETOOL) setdd "$@" $(IMPORTS) || ($(RM) "$@" && exit 1)
+	$(PETOOL) setdd "$@" $(TLS) || ($(RM) "$@" && exit 1)
+	$(PETOOL) setdd "$@" $(IAT) || ($(RM) "$@" && exit 1)
+	$(PETOOL) setc  "$@" .p_text 0x60000020 || ($(RM) "$@" && exit 1)
 	$(PETOOL) patch "$@" || ($(RM) "$@" && exit 1)
 	$(STRIP) -R .patch "$@" || ($(RM) "$@" && exit 1)
 	$(PETOOL) dump "$@"
--- inc/macros/patch.h
+++ inc/macros/patch.h
@@ -1,20 +1,21 @@
 
+#define SETINST(addr, inst)                         \
+    __asm (                                         \
+        ".section .patch,\"d0\";"                   \
+        ".long " #addr ";"                          \
+        ".long (2f - 1f);"                          \
+        "1:;"                                       \
+            inst ";"                                \
+        "2:;"                                       \
+        ".section .text;"                           \
+    )
+
 #define CLEAR(start, value, end)                    \
     __asm (                                         \
         ".section .patch,\"d0\";"                   \
         ".long " #start ";"                         \
         ".long " #end "-" #start ";"                \
         ".fill " #end "-" #start ", 1, " #value ";" \
-        ".section .text;"                           \
-    )
-
-#define LJMP(src, dst)                              \
-    __asm (                                         \
-        ".section .patch,\"d0\";"                   \
-        ".long " #src ";"                           \
-        ".long 5;"                                  \
-        ".byte 0xE9;"                               \
-        ".long " #dst "-" #src " - 5;"              \
         ".section .text;"                           \
     )
 
@@ -25,6 +26,16 @@
         ".long 2;"                                  \
         ".byte 0xEB;"                               \
         ".byte " #dst "-" #src " - 2;"              \
+        ".section .text;"                           \
+    )
+
+#define LJMP(src, dst)                              \
+    __asm (                                         \
+        ".section .patch,\"d0\";"                   \
+        ".long " #src ";"                           \
+        ".long 5;"                                  \
+        ".byte 0xE9;"                               \
+        ".long " #dst "-" #src " - 5;"              \
         ".section .text;"                           \
     )
 
@@ -49,29 +60,73 @@
         ".section .text;"                           \
     )
 
-#define SETDWORD(dst, value)                        \
+#define SETDWORD(addr, value)                       \
     __asm (                                         \
         ".section .patch,\"d0\";"                   \
-        ".long " #dst ";"                           \
+        ".long " #addr ";"                          \
         ".long 4;"                                  \
         ".long " #value ";"                         \
         ".section .text;"                           \
     )
 
-#define SETWORD(dst, value)                         \
+#define SETWORD(addr, value)                        \
     __asm (                                         \
         ".section .patch,\"d0\";"                   \
-        ".long " #dst ";"                           \
+        ".long " #addr ";"                          \
         ".long 2;"                                  \
         ".short " #value ";"                        \
         ".section .text;"                           \
     )
 
-#define SETBYTE(dst, value)                         \
+#define SETBYTE(addr, value)                        \
     __asm (                                         \
         ".section .patch,\"d0\";"                   \
-        ".long " #dst ";"                           \
+        ".long " #addr ";"                          \
         ".long 1;"                                  \
         ".byte " #value ";"                         \
         ".section .text;"                           \
     )
+    
+#define SETBYTES(addr, value)                       \
+    __asm (                                         \
+        ".section .patch,\"d0\";"                   \
+        ".long " #addr ";"                          \
+        ".long (2f - 1f);"                          \
+        "1:;"                                       \
+            ".ascii " #value ";"                    \
+        "2:;"                                       \
+        ".section .text;"                           \
+    )
+
+#define HOOK_1(addr)                                \
+    __asm (                                         \
+        ".section .patch,\"d0\";"                   \
+        ".long " #addr ";"                          \
+        ".long 5;"                                  \
+        ".byte 0xE9;"                               \
+        ".long _dest" #addr "-" #addr " - 5;"       \
+        ".section .text;"                           \
+    );                                              \
+    EXTERN_C void __attribute__((naked)) dest##addr()
+    
+#define HOOK_2(addr, end)                           \
+    CLEAR_INT((addr + 5), end);                     \
+    HOOK_1(addr)
+
+#define HOOK_X(x,A,B,FUNC, ...)  FUNC  
+#define HOOK(...)         HOOK_X(,##__VA_ARGS__,    \
+                               HOOK_2(__VA_ARGS__), \
+                               HOOK_1(__VA_ARGS__), \
+                               HOOK_0(__VA_ARGS__)  \
+                               )
+
+#define CLEAR_NOP(start, end) CLEAR(start, 0x90, end)
+#define CLEAR_INT(start, end) CLEAR(start, 0xCC, end)
+
+#define LJMP_NOP(start, end, dst)                   \
+    CLEAR_NOP((start + 5), end);                    \
+    LJMP(start, dst)
+
+#define LJMP_INT(start, end, dst)                   \
+    CLEAR_INT((start + 5), end);                    \
+    LJMP(start, dst)
